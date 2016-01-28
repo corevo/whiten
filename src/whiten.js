2c55ef679bee738f3f398854543532561b06a1d8
Tomer <gmtomer@gmail.com>
2016-01-28 12:15:22 +0200

    works hardcoded from npm

--- src/whiten.js
+++ src/whiten.js
@@ -2,12 +2,13 @@ import path from 'path';
 import fs from 'fs';
 import YAML from 'yamljs';
 
+const registries = require('require-all')(__dirname + '/registry');
 const defaultConfig = YAML.load('lib/config.yaml');
 
 function saveConfig(storagePath) {
     let config = Object.assign(defaultConfig, { storage: path.join(storagePath, 'storage') });
     let configPath = path.join(storagePath, 'config.yaml');
-    fs.writeFileSync(configPath, config);
+    fs.writeFileSync(configPath, YAML.stringify(config));
 
     return configPath;
 }
@@ -20,4 +21,6 @@ function setupEnv(port, config) {
 export default function whiten(savePath, registry, modules, storagePath, port) {
     let configPath = saveConfig(storagePath);
     setupEnv(port, configPath);
+    require('sinopia/lib/cli');
+    registries[registry].default(`http://localhost:${port}/`, path.join(storagePath, "temp"), modules);
 }
