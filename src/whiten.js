0f9507c3c9f471297741e60d0b3c8155fa2e811c
Tomer <gmtomer@gmail.com>
2016-01-28 11:25:23 +0200

    saveConfig refactor

--- src/whiten.js
+++ src/whiten.js
@@ -1,7 +1,17 @@
+import path from 'path';
+import fs from 'fs';
 import YAML from 'yamljs';
 
 const defaultConfig = YAML.load('lib/config.yaml');
 
-export default function whiten(path, registry, modules, storagePath, port) {
-    let config = Object.assign(defaultConfig, { storage: storagePath });
+function saveConfig(storagePath) {
+    let config = Object.assign(defaultConfig, { storage: path.join(storagePath, 'storage') });
+    let configPath = path.join(storagePath, 'config.yaml');
+    fs.writeFileSync(configPath, config);
+
+    return configPath;
+}
+
+export default function whiten(savePath, registry, modules, storagePath, port) {
+    let configPath = saveConfig(storagePath);
 }
