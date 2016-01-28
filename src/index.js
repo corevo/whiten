1a58517a4a63a35542e9c37faca415e3eb8d2065
Tomer <gmtomer@gmail.com>
2016-01-28 16:13:11 +0200

    function that will cover the whiten function

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import path from 'path';
+import os from 'os';
+import mkdirp from 'mkdirp';
+import md5 from 'md5';
+import magic from './whiten';
+
+export default function whiten(savePath, modules, registry) {
+    if (!registry) {
+        registry = "npm";
+    }
+    let tempDir = path.join(os.tmpdir(), "whiten-" + md5(new Date().toString()));
+    console.log("Downloading to " + tempDir);
+    mkdirp.sync(path.join(tempDir, "storage"));
+    mkdirp.sync(path.join(tempDir, "npm"));
+}
