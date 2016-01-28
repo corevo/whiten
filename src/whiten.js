cf773ebe43d0dac30f1c811848cef74cbf2d8716
Tomer <gmtomer@gmail.com>
2016-01-28 11:11:48 +0200

    fixed path

--- src/whiten.js
+++ src/whiten.js
@@ -1,6 +1,6 @@
 import YAML from 'yamljs';
 
-const defaultConfig = YAML.load('config.yaml');
+const defaultConfig = YAML.load('lib/config.yaml');
 
 export default function whiten(path, registry, modules, storagePath, port) {
     let config = Object.assign(defaultConfig, { storage: storagePath });
