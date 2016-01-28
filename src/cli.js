c67cf96bccc6ce13e90c40bc36c3e8f40129df15
Tomer <gmtomer@gmail.com>
2016-01-28 18:57:17 +0200

    cli yay

---
+++ src/cli.js
@@ -0,0 +1,19 @@
+import program from 'commander';
+
+program
+    .version(require('../package.json').version)
+    .usage('[-r] <module ...>')
+    .option('-r --registry [source]', 'Source to whiten from (npm), if none given npm will be assumed', /^(npm)$/i, 'npm')
+    .parse(process.argv);
+
+let modules = program.args;
+
+if (modules.constructor !== Array) {
+    console.error("Provide modules separated by spaces e.g. whiten babel react react-dom");
+    process.exit(1);
+}
+
+if (modules.length < 1) {
+    console.error("No modules were specified e.g. whiten babel react react-dom")
+    process.exit(1);
+}
