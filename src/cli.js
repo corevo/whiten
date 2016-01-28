70945126c6ea22441d8101b678876be205eaea37
Tomer <gmtomer@gmail.com>
2016-01-28 19:14:38 +0200

    show error in case of arguments failure

--- src/cli.js
+++ src/cli.js
@@ -3,18 +3,12 @@ import program from 'commander';
 process.title = "whiten";
 program
     .version(require('../package.json').version)
-    .usage('[-r] <module ...>')
+    .usage('[options] <module ...>')
     .option('-r --registry [source]', 'Source to whiten from (npm), if none given npm will be assumed', /^(npm)$/i, 'npm')
     .parse(process.argv);
 
 let modules = program.args;
 
-if (modules.constructor !== Array) {
-    console.error("Provide modules separated by spaces e.g. whiten babel react react-dom");
-    process.exit(1);
-}
-
-if (modules.length < 1) {
-    console.error("No modules were specified e.g. whiten babel react react-dom")
-    process.exit(1);
+if (modules.constructor !== Array || modules.length < 1) {
+    program.help();
 }
