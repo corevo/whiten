01fa5880a920b6083870f12da2eae1456bb03bcf
Tomer <gmtomer@gmail.com>
2016-01-28 16:24:19 +0200

    works with a random port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import os from 'os';
 import mkdirp from 'mkdirp';
 import md5 from 'md5';
+import randomPort from 'random-port';
 import magic from './whiten';
 
 export default function whiten(savePath, modules, registry) {
@@ -11,5 +12,8 @@ export default function whiten(savePath, modules, registry) {
     let tempDir = path.join(os.tmpdir(), "whiten-" + md5(new Date().toString()));
     console.log("Downloading to " + tempDir);
     mkdirp.sync(path.join(tempDir, "storage"));
-    mkdirp.sync(path.join(tempDir, "npm"));
+    mkdirp.sync(path.join(tempDir, "temp"));
+    randomPort(port => {
+        magic(savePath, registry, modules, tempDir, port);
+    });
 }
