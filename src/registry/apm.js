9735efb9d0a5acc2737ae1b61a2d387b44b581b0
Tomer <gmtomer@gmail.com>
2016-01-31 13:05:45 +0200

    works with array

--- src/registry/apm.js
+++ src/registry/apm.js
@@ -5,7 +5,7 @@ const apmApi = process.env.ATOM_API_URL || 'https://atom.io/api';
 
 export default function fetch(port, config, path, modules) {
     Promise.all(modules.map(module => (
-        request.get(`${apmApi}/packages/${modules[0]}`).promise()
+        request.get(`${apmApi}/packages/${module}`).promise()
     ))).map(extension => {
         extension = JSON.parse(extension.text);
         let latest = extension.versions[extension.releases.latest];
