7815ff632a27e0a54a68afec32fb4a877e732339
Tomer <gmtomer@gmail.com>
2016-01-31 12:57:15 +0200

    yeah map promises

--- src/registry/apm.js
+++ src/registry/apm.js
@@ -1,12 +1,9 @@
-import request from 'superagent';
+import request from 'superagent-bluebird-promise';
 
 const apmApi = process.env.ATOM_API_URL || 'https://atom.io/api';
 
 export default function fetch(port, config, path, modules) {
-    request.get(`${apmApi}/packages/${modules[0]}`).end((err, res) => {
-        if (!err) {
-            let extension = JSON.parse(res.text);
-            let latest = extension.versions[extension.releases.latest];
-        }
-    });
+    let packages = modules.map(module => (
+        request.get(`${apmApi}/packages/${modules[0]}`).promise()
+    ));
 }
