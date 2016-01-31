5330aea812a7d99be6ed0f0b9fcc55d487c9e5ab
Tomer <gmtomer@gmail.com>
2016-01-31 13:04:11 +0200

    mapping promises is so gud

--- src/registry/apm.js
+++ src/registry/apm.js
@@ -1,9 +1,13 @@
+import Promise from 'bluebird';
 import request from 'superagent-bluebird-promise';
 
 const apmApi = process.env.ATOM_API_URL || 'https://atom.io/api';
 
 export default function fetch(port, config, path, modules) {
-    let packages = modules.map(module => (
+    Promise.all(modules.map(module => (
         request.get(`${apmApi}/packages/${modules[0]}`).promise()
-    ));
+    ))).map(extension => {
+        extension = JSON.parse(extension.text);
+        let latest = extension.versions[extension.releases.latest];
+    });
 }
