346505f56fac9294df21d18dd540ca7eeac4574a
Tomer <gmtomer@gmail.com>
2016-01-31 15:05:35 +0200

    now returns an object of the tgz dist

--- src/registry/apm.js
+++ src/registry/apm.js
@@ -4,12 +4,21 @@ import request from 'superagent-bluebird-promise';
 const apmApi = process.env.ATOM_API_URL || 'https://atom.io/api';
 
 export default function fetch(port, config, path, modules) {
-    let nodeModules = Promise.all(modules.map(module => (
+    let { dependencies, extensions } = Promise.all(modules.map(module => (
         request.get(`${apmApi}/packages/${module}`).promise()
-    ))).reduce(dependencies, extension => {
+    ))).reduce(({ dependencies, extensions }, extension) => {
         extension = JSON.parse(extension.text);
         let pkg = extension.versions[extension.releases.latest];
         let deps = pkg.dependencies;
-        return [...dependencies, ...Object.keys(deps).map(dep => (`${dep}@${deps[dep]}`))];
-    }, []);
+        return {
+            dependencies: [...dependencies, ...Object.keys(deps).map(dep => (`${dep}@${deps[dep]}`))],
+            extensions: {
+                [`${pkg.name}@${pkg.version}`]: pkg.dist.tarball
+            }
+        }
+    }, {
+        dependencies: [],
+        extensions: {}
+    }).then(({ dependencies, extensions }) => {
+    });
 }
