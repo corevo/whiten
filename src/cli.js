406b41f56759b9e6f56ae221672b629ff48e08fc
Tomer <gmtomer@gmail.com>
2016-03-14 16:26:02 +0200

    whitens the current directory if no parameters are given

--- src/cli.js
+++ src/cli.js
@@ -14,12 +14,14 @@ program
 
 let modules = program.args;
 
-if (modules.constructor !== Array || modules.length < 1) {
+if (modules.constructor !== Array) {
     program.help();
 }
 
 whiten(modules, program.registry, (tar, cb) => {
-    tar.pipe(fs.createWriteStream(path.join(process.cwd(), modules.join(' ') + '.tar'))).on('finish', () => {
+    let name = modules.join(' ');
+    name = name ? name : path.basename(process.cwd());
+    tar.pipe(fs.createWriteStream(path.join(process.cwd(), name + '.tar'))).on('finish', () => {
         cb();
     });
 });
